63bc329ed04034baafa111c25752c3c1a0d17de9
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-08-30 18:29:40 +0200

    hided token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"2e1ea0b8e0aad26082cffd6273909cd4877c0624"
 };
 
 /*
