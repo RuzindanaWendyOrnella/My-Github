ba752eb1a164a3dd0efe5b45721f848065ba3661
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-01 00:42:25 +0200

    updated my file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
-import { DisplayerComponent } from './displayer/displayer.component';
+
 
 
 const routes: Routes = [
-  // { path: 'pages', component:PagesComponent},
-  // { path: 'pages/:id', component:PagesComponent},
+  { path: 'pages', component:PagesComponent},
+  { path: 'pages/:id', component:PagesComponent},
   // { path:'displayer',component:DisplayerComponent},
   // { path: '', redirectTo:"/pages", pathMatch:"full"}
 ];
