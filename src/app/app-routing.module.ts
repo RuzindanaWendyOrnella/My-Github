248aa5feaa166d4763a25aedf5ee68450f70db55
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-08-31 20:46:05 +0200

    added paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
+import { DisplayerComponent } from './displayer/displayer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'pages', component:PagesComponent},
+  // { path: 'pages/:id', component:PagesComponent},
+  // { path:'displayer',component:DisplayerComponent},
+  // { path: '', redirectTo:"/pages", pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
