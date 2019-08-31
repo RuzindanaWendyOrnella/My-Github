8a0c0338adf8b185b15fc39120ff6a75ac8a1685
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-08-31 20:47:24 +0200

    added displayer import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
+import { DisplayerComponent } from './displayer/displayer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent
+    PagesComponent,
+    DisplayerComponent
     
   ],
   imports: [
