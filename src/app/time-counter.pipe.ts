cf9439e71e8d535483f1951eceb8eae58601bf2b
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-01 03:35:33 +0200

    created pipe

---
+++ src/app/time-counter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeCounter'
+})
+export class TimeCounterPipe implements PipeTransform {
+
+  transform(value: any): number {
+    let today:Date = new Date(); //get current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
+    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
+    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+    var timeCounter = dateDifferenceSeconds/secondsInDay;
+  return timeCounter;
+  }
+
+
+}
