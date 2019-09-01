a5a60d7f72e77a45f0497e7c2b056680d1e6c30a
RuzindanaWendyOrnella <ruzindanawendy@gmail.com>
2019-09-01 03:38:53 +0200

    added date in constructor

--- src/app/github-class/github.ts
+++ src/app/github-class/github.ts
@@ -1,3 +1,3 @@
 export class Github {
-    constructor(public login:string,public id:number,public avatar_url:string,public public_repos:number,public followers:number,public following:number){}
+    constructor(public login:string,public id:number,public avatar_url:string,public public_repos:number,public followers:number,public following:number,public completeDate: Date){}
 }
